2ef260636b1886328763c821c9c49fadefbc9d0c
giovansa <giovansaputra19@gmail.com>
2017-09-13 15:47:47 +0700

    update-bug

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -32,12 +32,36 @@ module.exports = {
 	index: function(req, res, next){
 		User.find(function foundUser(err, users){
 			if(err)return next(err);
-
 			res.view('user/index',{
 				users:users
 			});
 		});
 	},
 
+	update: function(req, res, next){
+		User.findOne(req.param('id'), function(err, user){
+			if(err) return next(err);
+			if(!user)return next();
+			res.view({
+				user:user
+			});
+		});
+	},
+
+	updating: function(req, res, next){
+		var n_user = req.allParams();
+		console.log(n_user);
+		User.findOne(n_user.id, function(err, user){
+			if(err)return next(err);
+			if(!user)return next();
+
+			user.update({username:n_user.username}, {user_gender:n_user.user_gender}).exec(function result(err, updated){
+				//console.log(err);
+				console.log(updated.username)
+				if(err)return next(err);
+				res.redirect('user/index');
+			});
+		});
+	},
 
 };
